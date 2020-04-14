dbf4fd5b8f43d3441ac5a61494d8f6630ffc8921
Neel Mazumdar <neelmazumdar7@gmail.com>
2020-04-14 00:52:41 -0400

    did stuff

--- api/server.js
+++ api/server.js
@@ -1,9 +1,16 @@
 const express = require("express");
 
 const db = require("../data/dbConfig.js");
+const AccRouter = require('../accounts/acc-router');
 
 const server = express();
 
 server.use(express.json());
+
+server.use("/api/accounts", AccRouter);
+
+server.get("/", (req, res) => {
+  res.status(200).json({ account: "up" });
+});
 
 module.exports = server;
